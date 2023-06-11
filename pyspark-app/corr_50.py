e15e83f0530d248858c6808ce649093ce3bc9962
huibaobao <hjessica@gmail.com>
2023-06-11 19:04:58 -0400

    remove corr code

--- pyspark-app/corr_50.py
+++ pyspark-app/corr_50.py
@@ -4,29 +4,21 @@ from pyspark.sql.functions import corr, lit
 
 spark = SparkSession.builder.getOrCreate()
 inputs = spark.sparkContext.parallelize(range(50))
-
+print("-----inputs are created------")
 def generate_rows(input_val):
     rows = []
-    for i in range(10000):
-        rows.append(Row(value=float(input_val * 10000 + i)))
+    for i in range(1000):
+        rows.append(Row(value=float(input_val * 1000 + i)))
     return rows
 
+# Calculate the correlation between columns
 df = inputs.flatMap(generate_rows).toDF()
-
+print("=========data frame is created==========")
 # Create new columns based on the `value` column
 df = df.withColumn('double_value', df['value'] * 2)
 df = df.withColumn('triple_value', df['value'] * 3)
 df = df.withColumn('squared_value', df['value'] ** 2)
 
-# Calculate the correlation between columns
-corr_value_double = df.select(corr('value', 'double_value')).first()[0]
-corr_value_triple = df.select(corr('value', 'triple_value')).first()[0]
-corr_value_squared = df.select(corr('value', 'squared_value')).first()[0]
-
-# Add correlation values as new columns
-df = df.withColumn('corr_value_double', lit(corr_value_double))
-df = df.withColumn('corr_value_triple', lit(corr_value_triple))
-df = df.withColumn('corr_value_squared', lit(corr_value_squared))
 
 print("*****************number of rows****************", df.count())
 # Show the updated DataFrame
